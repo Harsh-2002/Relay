1321824403e266e53ce51c4ca1eb6a46cc95584a
Anurag Vishwakarma <65254552+Harsh-2002@users.noreply.github.com>
2026-02-28 17:07:42 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/ocbot",
+  basePath: "/relay",
   images: {
     unoptimized: true,
   },
